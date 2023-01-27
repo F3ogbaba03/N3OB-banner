9a0abda0b6c1f980266566e5a9c1991e43c020ec
F3ogbaba03 <122087854+F3ogbaba03@users.noreply.github.com>
2023-01-27 12:59:43 +0600

    Update N3ob-banner.sh

--- N3ob-banner.sh
+++ N3ob-banner.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 echo ""
-read -p $' \e[0m\e[1;92m Input your Username : \e[0m\e[1;96m\en' user
+read -p $' \e[0m\e[1;92m Input your name bro : \e[0m\e[1;96m\en' user
 echo ""
 echo ""
 echo -e  $g "একটু অপেক্ষা কর নলা "
@@ -16,7 +16,7 @@ pip install termcolor
 pkg install ncurses-utils -y 
 pkg install ruby -y 
 gem install lolcat 
-figlet -f big পাগল হইসোস কেনো | lolcat
+figlet -f small পাগল হইসোস কেনো | lolcat
 git clone https://github.com/F3ogbaba03/N3ob-banner
 sed 's+DHB+'$user'+g' N3ob-banner/bash.bashrc > /data/data/com.termux/files/usr/etc/bash.bashrc
 sed 's+DHB+'$user'+g' N3ob-banner/wlc.py > /data/data/com.termux/files/usr/etc/wlc.py
